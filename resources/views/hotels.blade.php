8fd0319a2b9ef7cb77fdf52995737bc2414d2bd8
Dylaanomer <70571414+Dylaanomer@users.noreply.github.com>
2022-03-07 22:07:49 +0100

    2nd

---
+++ resources/views/hotels.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <h1>Hotel Hotels Hotel</h1>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KamerController;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,19 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/pages',
+[PagesController::class, 'index']);
+
+Route::get('/kamers',
+    [KamerController::class, 'index']);
+
+Route::resource(name: 'boekingen',controller: 'BoekingController');
+
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
