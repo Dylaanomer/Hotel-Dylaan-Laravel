82386bdb2325fa993b3f4f22e9673f4ddb03318c
Dylaanomer <70571414+Dylaanomer@users.noreply.github.com>
2022-03-14 23:10:28 +0100

    WERKEND CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KamerController;
-use App\Http\Controllers\PagesController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/my-page', function () {
-    return view('my-page');
-});
-
-Route::get('/create', function () {
-    return view('kamer.create');
-});
-
-Route::get('/index', function () {
-    return view('kamer.index');
-});
-
-Route::get('/voorbeeld', function () {
-    return view('pages.voorbeeld');
-});
-
-Route::get('/hotels', function () {
-    return view('hotels');
+    return view('welcome');
 });
 
 
+Route::resource('products', ProductController::class);
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
